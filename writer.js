e191d41f0d7cde5108ecab787b0220b6afb2f3a9
mrdarip <darioelas2005@gmail.com>
2024-07-27 14:21:47 +0200

    Added code to build from

--- writer.js
+++ writer.js
@@ -1,7 +1,41 @@
 // write with your own font!!
-const width = 125;
-const height = 125;
+const canvasSize = { x: 125, y: 125 };
 
-setDocDimensions(width, height);
+setDocDimensions(canvasSize.x, canvasSize.y);
+
+const charSize = { x: 5, y: 5 }; 
+const charSpacing = { x: 1, y: 1 };
+
+const text = "hello world";
+
+// Define your own font here, with the characters you want to draw as keys and the lines you want to draw as values
+const font = {
+    a: [],
+    b: [],
+    c: [],
+    d: [],
+    e: [],
+    f: [],
+    g: [],
+    h: [],
+    i: [],
+    j: [],
+    k: [],
+    l: [],
+    m: [],
+    n: [],
+    o: [],
+    p: [],
+    q: [],
+    r: [],
+    s: [],
+    t: [],
+    u: [],
+    v: [],
+    w: [],
+    x: [],
+    y: [],
+    z: []
+};
 
 drawLines(polyline);
